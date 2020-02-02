d53181cabfb731f23ad0188f2f5da927ae0631a6
pangari <60435424+pangari@users.noreply.github.com>
2020-02-02 22:45:26 +1100

    Create example.C

---
+++ EncodeCStr/example.C
@@ -0,0 +1,21 @@
+#include "EncodeCStr.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+    const char in[] = "Hello\tWorld\n";
+    char out[sizeof(in) * 4];
+
+    PrintBufferAsCStr(stdout, in, sizeof(in));
+
+    ConvertBufferAsCStr(out, in, sizeof(in));
+    fprintf(stdout, "\n[%s]\n", out);
+
+    DecodeCStr(out);
+    fprintf(stdout, "\n[%s]\n", out);
+
+    return 0;
+}
