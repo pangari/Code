bac21484fdafca5d99a3c50639d21a433776ec74
pangari <60435424+pangari@users.noreply.github.com>
2020-02-02 23:16:13 +1100

    Update EncodeCStr.C

--- EncodeCStr/EncodeCStr.C
+++ EncodeCStr/EncodeCStr.C
@@ -1,5 +1,5 @@
 /*
-Copyright (C) <2020> <duncan.forster@mac.com>
+Copyright (C) <2005-2020> <duncan.forster at mac dot com>
 
 Permission to use, copy, modify, and/or distribute this software for any purpose
 with or without fee is hereby granted.
